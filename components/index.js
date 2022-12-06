f8920c7a149b66f3ce512b4674670bb24fd2815d
arby usman <91175559+Arbyusman@users.noreply.github.com>
2022-12-07 02:47:33 +0800

    update

--- components/index.js
+++ components/index.js
@@ -5,6 +5,5 @@ import Banner from "./banner";
 import SlideShow from "./slideshow";
 import UserProfile from "./userProfile";
 import History from "../pages/history";
-import adminSidebar from "./adminSidebar";
 
 export { NavbarComponent,Footer,WhyUs,Banner,SlideShow,UserProfile,History };
