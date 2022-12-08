5d878e78db1480aeebbc13597377edf1a6de11a2
Dinniatul <kundinnii29@gmail.com>
2022-12-08 13:44:31 +0700

    add admin Dashboard

--- components/index.js
+++ components/index.js
@@ -4,7 +4,11 @@ import WhyUs from "./whyus";
 import Banner from "./banner";
 import SlideShow from "./slideshow";
 import UserProfile from "./userProfile";
+import History from "../pages/history";
+import SideBar from "./admin/SideBar";
+import TopBar from "./admin/TopBar";
+import Layout from "./admin/Layout";
 import HistoryBooking from "./historyBooking";
 import SearcResult from "./searchResult";
-import ConfirmBooking from "./confirmBooking";
-export { NavbarComponent,Footer,WhyUs,Banner,SlideShow,UserProfile,HistoryBooking,SearcResult,ConfirmBooking};
+
+export { NavbarComponent,Footer,WhyUs,Banner,SlideShow,UserProfile,History,TopBar,Layout,SideBar,HistoryBooking,SearcResult};
