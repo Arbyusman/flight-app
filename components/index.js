79e4a1746685034983e8e983c33eb29216df6a74
Arbyusman <acingman13@gmail.com>
2022-12-13 02:26:15 +0800

    fix err

--- components/index.js
+++ components/index.js
@@ -4,11 +4,10 @@ import WhyUs from "./whyus";
 import Banner from "./banner";
 import SlideShow from "./slideshow";
 import UserProfile from "./userProfile";
-import History from "../pages/history";
 import SideBar from "./admin/SideBar";
 import TopBar from "./admin/TopBar";
 import Layout from "./admin/Layout";
 import HistoryBooking from "./historyBooking";
 import SearcResult from "./searchResult";
 
-export { NavbarComponent,Footer,WhyUs,Banner,SlideShow,UserProfile,History,TopBar,Layout,SideBar,HistoryBooking,SearcResult};
+export { NavbarComponent,Footer,WhyUs,Banner,SlideShow,UserProfile,TopBar,Layout,SideBar,HistoryBooking,SearcResult};
