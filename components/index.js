6ffc2063a2e878f5364b98a1d11f07f6f948e283
Dinniatul <kundinnii29@gmail.com>
2022-12-13 01:06:25 +0700

    fix index.js

--- components/index.js
+++ components/index.js
@@ -4,16 +4,13 @@ import WhyUs from "./whyus";
 import Banner from "./banner";
 import SlideShow from "./slideshow";
 import UserProfile from "./userProfile";
-import History from "../pages/history";
+
 import SideBar from "./admin/SideBar";
 import TopBar from "./admin/TopBar";
 import Layout from "./admin/Layout";
-import History from "../pages/history";
-import SideBar from "./admin/SideBar";
-import TopBar from "./admin/TopBar";
-import Layout from "./admin/Layout";
+
 import HistoryBooking from "./historyBooking";
 import SearcResult from "./searchResult";
 import ConfirmBooking from "./confirmBooking";
 
-export { NavbarComponent, Footer, WhyUs, Banner, SlideShow, UserProfile, History, TopBar, Layout, SideBar, HistoryBooking, SearcResult, ConfirmBooking };
+export { NavbarComponent, Footer, WhyUs, Banner, SlideShow, UserProfile, TopBar, Layout, SideBar, HistoryBooking, SearcResult, ConfirmBooking };
