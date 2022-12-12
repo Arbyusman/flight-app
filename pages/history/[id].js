4739d1953517452bba4183fa8a64dac616b44323
Arbyusman <acingman13@gmail.com>
2022-12-13 00:03:49 +0800

    add  protected routtes

--- pages/history/[id].js
+++ pages/history/[id].js
@@ -1,11 +1,28 @@
 import { NavbarComponent, Footer, HistoryBooking } from "../../components";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function History() {
-  return (
-    <div>
-      <NavbarComponent />
-      <HistoryBooking />
-      <Footer />
-    </div>
-  );
+  const [token, setToken] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const tokenprofile = localStorage.getItem("token");
+    if (!tokenprofile) router.push("/login");
+
+    setToken(tokenprofile);
+  }, []);
+
+  if (!token) {
+    return;
+  } else {
+    return (
+      <div>
+        <NavbarComponent />
+        <HistoryBooking />
+        <Footer />
+      </div>
+    );
+  }
 }
