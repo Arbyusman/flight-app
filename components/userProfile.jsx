2c9475083a43a227c6e12eb4a841a2e7e4d5e80a
Arbyusman <acingman13@gmail.com>
2022-12-15 03:28:34 +0800

    add animation spin

--- components/userProfile.jsx
+++ components/userProfile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import Image from "next/image";
-import imageProfil from "../public/images/mega.jpg";
+import { Modal } from "flowbite-react";
 import { FaUserEdit } from "react-icons/fa";
 import { BsTrash } from "react-icons/bs";
 import { BiSave } from "react-icons/bi";
@@ -22,6 +21,8 @@ export default function UserProfile() {
   const [phone, setPhone] = useState("");
 
   const [loading, setLoading] = useState(true);
+
+  const [saveLoading, setSaveLoading] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -44,14 +45,14 @@ export default function UserProfile() {
 
   async function handelUpdate() {
 
+    setSaveLoading(true);
+
     const body = new FormData();
-
-    body.append("firstName",firstName);
-    body.append("lastName",lastName);
-    body.append("phone",phone);
-    body.append("photo",photo);
-    body.append("address",address);
-
+    body.append("firstName", firstName);
+    body.append("lastName", lastName);
+    body.append("phone", phone);
+    body.append("photo", photo);
+    body.append("address", address);
 
     const id = localStorage.getItem("id");
     const token = localStorage.getItem("token");
@@ -69,10 +70,13 @@ export default function UserProfile() {
       throw err;
     });
 
+    setTimeout(() => {
+      setSaveLoading(false);
+    }, 1500);
+
     const data = await response.json();
 
     if (data.status === "OK") {
-      alert("data berhasil di ubah");
       setEditProfile(false);
     }
 
@@ -80,7 +84,27 @@ export default function UserProfile() {
   }
 
   if (loading) {
-    return <div>loading</div>;
+    return (
+      <div>
+        {" "}
+        <svg
+          role="status"
+          class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="#1C64F2"
+          />
+        </svg>
+      </div>
+    );
   } else {
     return (
       <div className=" flex justify-center mt-5  ">
@@ -251,6 +275,7 @@ export default function UserProfile() {
             </div>
           </div>
         ) : (
+          // edit profile
           <div className="w-full mx-3 rounded-md md:w-1/2 items-center shadow-xl bg-white">
             <div className="flex shadow-md w-full ">
               <button className=" rounded-md ml-5 ">
@@ -425,7 +450,25 @@ export default function UserProfile() {
                     >
                       <p className="text-lg font-bold text-white  px-3 py-3 flex items-center justify-center gap-2">
                         {" "}
-                        <BiSave className="text-white" />
+                        {!saveLoading && <BiSave className="text-white" />}
+                        {saveLoading && (
+                          <svg
+                            role="status"
+                            class="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                              fill="currentColor"
+                            />
+                            <path
+                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                              fill="#1C64F2"
+                            />
+                          </svg>
+                        )}
                         Save
                       </p>
                     </button>
