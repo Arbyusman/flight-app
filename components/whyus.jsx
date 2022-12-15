241bca30baf0a86adcb1a50693c3aa54f83ca843
Arbyusman <acingman13@gmail.com>
2022-12-15 21:20:28 +0800

    fix whyus section

--- components/whyus.jsx
+++ components/whyus.jsx
@@ -1,72 +1,62 @@
 export default function WhyUs() {
   return (
-    <div id="whyus" className="grid justify-center items-center my-5 mx-5 ">
-      <h5 className="flex my-6 sm:text-2xl items-center  justify-center font-semibold tracking-tight text-gray-800 ">
-        Why Take Off ?
-      </h5>
-      <div className="container justify-center md:grid-cols-2 text-sm p-0 grid  gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <div
-          href="#"
-          className="flex-row  p-6 md:px-10 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
-        >
-          <img
-            className="w-24 md:w-36"
-            src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
-            alt="img why us"
-          />
-          <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
+    <div id="whyus" className="flex justify-center items-center  my-8 ">
+      <div className="lg:w-8/12 md:w-10/12">
+        <h5 className="flex my-6 sm:text-2xl items-center  justify-center font-semibold tracking-tight text-gray-800 ">
+          Why Take Off ?
+        </h5>
+        <div className="flex-row md:flex items-center justify-center text-center md:gap-7 ">
+          <div
+            href="#"
+            className="flex-row p-6 md:w-80 md:h-80 md:px-2 lg:px-7 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
+          >
+            <div className="w-full justify-center flex items-center">
+              <img
+                className="w-24 md:w-36"
+                src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
+              />
+            </div>
+            <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
             Simplify Your Booking Experience
-          </h5>
-          <p className="font-normal text-left text-gray-800">
+            </h5>
+            <p className="font-normal text-center text-gray-800">
             Feel the flexibility and simplicity throughout your booking process
-          </p>
-        </div>
-        <div
-          href="#"
-          className="block p-6 md:px-10 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
-        >
-          <img
-            className="w-24 md:w-36"
-            src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
-          />
-          <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
-            Worlds Local Booking Excitement
-          </h5>
-          <p className="font-normal text-left text-gray-800">
-            Stress-free booking experience with local payment, currency, and
-            language
-          </p>
-        </div>
-        <div
-          href="#"
-          className="block p-6 md:px-10 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
-        >
-          <img
-            className="w-24 md:w-36"
-            src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
-          />
-          <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
-            Exclusive Offer Everyday
-          </h5>
-          <p className="font-normal text-left text-gray-800">
-            Various daily promo with competitive price for all travelers
-          </p>
-        </div>
-        <div
-          href="#"
-          className="block p-6 md:px-10 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
-        >
-          <img
-            className="w-24 md:w-36"
-            src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
-          />
-          <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
+            </p>
+          </div>
+          <div
+            href="#"
+            className="flex-row p-6 md:w-80 md:h-80 md:px-2 lg:px-7 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
+          >
+            <div className="w-full justify-center flex items-center">
+              <img
+                className="w-24 md:w-36"
+                src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
+              />
+            </div>
+            <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
             Online Booking Expert
-          </h5>
-          <p className="font-normal text-left text-gray-800">
-            Together with our credible partners, fulfilling countless travelers
-            needs since 2011
-          </p>
+            </h5>
+            <p className="font-normal text-center text-gray-800">
+            Together with our credible partners, fulfilling countless travelers needs since 2011
+            </p>
+          </div>
+          <div
+            href="#"
+            className="flex-row p-6 md:w-80 md:h-80 md:px-2 lg:px-7 md:py-5 h-100 max-w-md  border justify-center items-center rounded-md shadow-md "
+          >
+            <div className="w-full justify-center flex items-center">
+              <img
+                className="w-24 md:w-36"
+                src="https://cdn.airpaz.com/images/illustrations/simplifyBooking.svg"
+              />
+            </div>
+            <h5 className="mb-2 text-center sm:text-2xl font-bold tracking-tight text-gray-800 ">
+              Exclusive Offer Everyday
+            </h5>
+            <p className="font-normal text-center text-gray-800">
+              Various daily promo with competitive price for all travelers
+            </p>
+          </div>
         </div>
       </div>
     </div>
