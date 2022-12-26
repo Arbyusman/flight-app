a43484a14ba4a24c9d4a472aaa1c40af4d8fd0ad
Arbyusman <acingman13@gmail.com>
2022-12-27 03:39:11 +0800

    integrasi API Transaction

--- components/confirmFlight.jsx
+++ components/confirmFlight.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import { IoAirplaneOutline } from "react-icons/io5";
 import { BsPerson } from "react-icons/bs";
-import { IoIosTimer } from "react-icons/io";
 import { MdOutlineLuggage } from "react-icons/md";
-import { BiJoystick } from "react-icons/bi";
 import { GiBackpack } from "react-icons/gi";
-import { Button } from "flowbite-react";
-import Image from "next/image";
 
 export default function ConfirmFlight() {
   const router = useRouter();
   const { id } = router.query;
 
-  const [data, setData] = useState([]);
+  const [totalPrice, setTotalPrice] = useState("");
 
+  const [dataTicket, setDataTicket] = useState([]);
+  const [flight, setFlight] = useState([]);
+  const [plane, setPlane] = useState([]);
+  const [from, setFrom] = useState([]);
+  const [to, setTo] = useState([]);
+
+  const [userId, setUserId] = useState("");
   const [email, setEmail] = useState("");
   const [firstName, setfirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -41,6 +45,7 @@ export default function ConfirmFlight() {
       .then((res) => res.json())
 
       .then((data) => {
+        setUserId(data.data.id);
         setEmail(data.data.email);
         setfirstName(data.data.firstName);
         setLastName(data.data.lastName);
@@ -59,10 +64,47 @@ export default function ConfirmFlight() {
       .then((res) => res.json())
 
       .then((data) => {
-        setData(data.data);
-        console.log("ticket", data.data);
+        setDataTicket(data.data);
+        setFlight(data.data.Flight);
+        setPlane(data.data.Flight.Plane);
+        setFrom(data.data.Flight.from);
+        setTo(data.data.Flight.to);
       });
   };
+
+  async function handelBooking() {
+    const ticket_id = dataTicket.id;
+    const user_id = userId;
+    const total = dataTicket.price;
+    const promo_id = 1;
+    const body = { user_id, ticket_id, total, promo_id };
+
+    whoami();
+    const token = localStorage.getItem("token");
+    if (!token) router.push("/login");
+    const response = await fetch(
+      `${process.env.API_ENDPOINT}api/v1/transaction`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+        body: JSON.stringify(body),
+      }
+    ).catch((err) => {
+      throw err;
+    });
+
+    const data = await response.json();
+    console.log("book", data);
+
+    if (data.status === "OK") {
+      alert("berhasil ");
+    }
+  }
 
   return (
     <div className="justify-center items-center flex">
@@ -205,22 +247,24 @@ export default function ConfirmFlight() {
                   <div className="p-7 ">
                     <div className="flex justify-between md:flex-row text-sm font-thin my-1">
                       <p>Depart Flight</p>
-                      <p>
-                        {new Date(data.Flight.departure_time).toDateString()}
-                      </p>
+                      {new Date(flight.departure_time).toLocaleString(
+                        "default",
+                        { month: "long" }
+                      )}{" "}
+                      {new Date(flight.departure_time).getFullYear()}
                     </div>
                     <div className="flex justify-between md:flex-row  text-sm font-thin my-1">
                       <p>Plane</p>
                       <figure className="max-w-md">
                         <Image
                           className="w-10 lg:w-12 flex "
-                          src={item.Ticket.photo}
+                          src={dataTicket.photo}
                           alt="logo penerbangan"
                           width={50}
                           height={50}
                         ></Image>
                         <figcaption className="mt-2 text-xs md:text-center text-gray-500 dark:text-gray-400">
-                          {data.Flight.Plane.name}
+                          {plane.name}
                         </figcaption>
                       </figure>
                     </div>
@@ -229,40 +273,51 @@ export default function ConfirmFlight() {
                     <div className="flex w-full justify-between    gap-2 text-gray-600 tracking-wide my-3 antialiased">
                       <div className="flex  gap-4 items-center">
                         <div>
-                          <p className="font-bold text-xl">10 : 30</p>
-                          <p className="text-sm">9 Dec 2022</p>
+                          <p className="font-bold text-xl">
+                            {new Date(flight.departure_time).getHours()}
+                            {" : "}
+                            {new Date(flight.departure_time).getMinutes()}
+                          </p>
+                          <p className="text-sm">
+                            {new Date(flight.departure_time).toDateString()}
+                          </p>
                         </div>
                         <div className=" w-36 lg:w-56">
                           <p className="text-md font-semibold">
-                            {data.Flight.from.city} ({" "}
-                            {data.Flight.from.city_code} ){" "}
+                            {from.city} ( {from.city_code} ){" "}
                           </p>
-                          <p className="text-sm">{data.Flight.from.name}</p>
+                          <p className="text-sm">{from.name}</p>
                         </div>
                       </div>
 
                       <div className="flex gap-4 items-center">
                         <div>
-                          <p className="font-bold text-xl">11 : 30</p>
-                          <p className="text-sm">9 Dec 2022</p>
+                          <p className="font-bold text-xl">
+                            {new Date(flight.arrival_time).getHours()}
+                            {" : "}
+                            {new Date(flight.arrival_time).getMinutes()}
+                          </p>
+                          <p className="text-sm">
+                            {new Date(flight.arrival_time).toDateString()}
+                          </p>
                         </div>
                         <div className=" w-36 lg:w-56">
                           <p className="text-md font-semibold">
-                            {data.Flight.to.city} ( {data.Flight.to.city_code} ){" "}
+                            {to.city} ( {to.city_code} ){" "}
                           </p>
-                          <p className="text-sm">{data.Flight.to.name}</p>
+                          <p className="text-sm">{to.name}</p>
                         </div>
                       </div>
                     </div>
                     <div className="gap-4  text-gray-600 tracking-wide antialiased text-sm my-3">
                       <hr></hr>
-                      <div className="flex gap-3 items-center my-1 ">
-                        <GiBackpack className="" />
-                        <p>Cabin Baggage {data.cabin_baggage}</p>
+                      <div className="flex gap-3 items-center my-1 lg:my-3 ">
+                        <GiBackpack className="text-xl text-green-500" />
+                        <p>Cabin Baggage {dataTicket.cabin_baggage}</p>
                       </div>
-                      <div className="flex gap-3 items-center my-1">
-                        <MdOutlineLuggage className="t" />
-                        <p>Baggage {data.baggage}</p>
+                      <div className="flex gap-3 items-center my-1 lg:my-3">
+                        <MdOutlineLuggage className="text-xl text-blue-500" />
+                        <p>Baggage {dataTicket.baggage}</p>
                       </div>
                     </div>
                   </div>
@@ -287,36 +342,42 @@ export default function ConfirmFlight() {
                 <div className="font-semibold gap-5 my-2 text-base flex justify-between">
                   <div className="flex items-center gap-2 ">
                     <p>Depart</p>
-                    <p>KDI</p>
+                    <p>{from.city_code}</p>
                     <IoAirplaneOutline />
-                    <p>UPG</p>
+                    <p>{to.city_code}</p>
                   </div>
-                  <p>RP {data.price}</p>
+                  <p>RP {dataTicket.price}</p>
                 </div>
                 <div className="flex justify-between text-sm font-thin my-1">
                   <p>Adult x 1</p>
                   <p>RP 678.000</p>
                 </div>
-                <div className="flex justify-between text-sm font-thin my-1">
-                  <input type="text"></input>
+                {/* <div className="flex justify-between text-sm font-thin my-1">
                   <Button></Button>
-                </div>
+                  <input type="text"></input>
+                </div> */}
                 <hr />
                 <div className="flex justify-between text-lg font-bold tracking-wider my-2">
                   <p>total Price</p>
-                  <p>RP 678.000</p>
+                  <p
+                    value={dataTicket.price}
+                    onChange={(e) => setTotalPrice(e.target.value)}
+                  >
+                    RP {dataTicket.price}
+                  </p>
                 </div>
               </div>
             </div>
             <div className="flex justify-center items-center">
-              <a href="confirm/book">
+              <div>
                 <button
+                  onClick={handelBooking}
                   type="button"
                   className="focus:outline-none lg:w-full mt-2 text-white bg-red-700 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-semibold antialiased tracking-wide rounded-md text-md  px-5 py-2.5 mr-2 mb-2 "
                 >
-                  Continue Booking
+                  Booking Now
                 </button>
-              </a>
+              </div>
             </div>
           </div>
         </div>
